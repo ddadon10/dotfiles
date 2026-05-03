2150118947c3fa3b8fcef76617859323a7542d54
David Dadon <david@dadon.fr>
2026-05-03 12:51:26 +0000

    Add gopls proxy VS Code extension

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -16,6 +16,7 @@ exec code \
   --host 0.0.0.0 \
   --port 8000 \
   --commit-id "$(< /opt/vscode-cli/serve-web/commit-id)" \
+  --enable-proposed-api local.gopls-proxy \
   --without-connection-token \
   --accept-server-license-terms \
   --server-data-dir /opt/vscode-server \
