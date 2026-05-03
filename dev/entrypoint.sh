901ff3421b091457cd42f562034c04d8d21f9f1f
David Dadon <david@dadon.fr>
2026-05-03 09:46:17 +0000

    Preload VS Code web server assets

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -8,16 +8,14 @@ if [[ "$#" -gt 0 ]]; then
   exec "$@"
 fi
 
-mkdir -p /workspace /opt/vscode-user-data /opt/vscode-extensions /opt/vscode-server /opt/vscode-cli
+mkdir -p /workspace /opt/vscode-server/data/User /opt/vscode-server/extensions /opt/vscode-cli
 export VSCODE_CLI_DATA_DIR=/opt/vscode-cli
 
 exec code \
-  --no-sandbox \
-  --user-data-dir /opt/vscode-user-data \
-  --extensions-dir /opt/vscode-extensions \
   serve-web \
   --host 0.0.0.0 \
   --port 8000 \
+  --commit-id "$(< /opt/vscode-cli/serve-web/commit-id)" \
   --without-connection-token \
   --accept-server-license-terms \
   --server-data-dir /opt/vscode-server \
