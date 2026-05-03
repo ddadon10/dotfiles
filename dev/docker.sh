646e505e5f0b6cc9d07553c27d4302bb795c1c9a
David Dadon <david@dadon.fr>
2026-05-03 08:23:56 +0000

    Add gopls web forwarding helper

--- dev/docker.sh
+++ dev/docker.sh
@@ -17,8 +17,9 @@ docker_args=(
     --env "GIT_USER_NAME=${git_name}"
     --env "GIT_USER_EMAIL=${git_email}"
     --publish "127.0.0.1:8000:8000"
-    --mount type=bind,src="${PWD}",dst=/workspace
-    --mount type=bind,src="${HOME}/.codex",dst=/root/.codex
+    --publish "127.0.0.1:18080:18080"
+    --mount "type=bind,src=${PWD},dst=/workspace"
+    --mount "type=bind,src=${HOME}/.codex,dst=/root/.codex"
     --workdir /workspace
-  )
+)
 docker run "${docker_args[@]}" "${name}" "$@"
