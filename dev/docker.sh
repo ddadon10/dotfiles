6408da1b894cdba0108d53944d75d25b8cf283e3
David Dadon <david@dadon.fr>
2026-05-03 08:56:33 +0000

    Build dev image from dev directory

--- dev/docker.sh
+++ dev/docker.sh
@@ -7,7 +7,7 @@ name="${DEV_NAME:-dev}"
 
 mkdir -p "${HOME}/.codex"
 
-docker build --file dev/Dockerfile --tag "${name}" .
+docker build --tag "${name}" .
 
 docker_args=(
     --rm
@@ -18,7 +18,7 @@ docker_args=(
     --env "GIT_USER_EMAIL=${git_email}"
     --publish "127.0.0.1:8000:8000"
     --publish "127.0.0.1:18080:18080"
-    --mount "type=bind,src=${PWD},dst=/workspace"
+    --mount "type=bind,src=${PWD%/*},dst=/workspace"
     --mount "type=bind,src=${HOME}/.codex,dst=/root/.codex"
     --workdir /workspace
 )
