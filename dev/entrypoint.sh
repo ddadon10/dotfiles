ed4f1ae04a7c7e340ec050c612efd9aa58728c09
David Dadon <david@dadon.fr>
2026-05-27 11:36:52 +0000

    Simplify dev entrypoint startup behavior

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -4,18 +4,11 @@ set -Eeuo pipefail
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
 
-if [[ "$#" -gt 0 ]]; then
-  exec "$@"
-fi
-
-mkdir -p /workspace /opt/vscode-server/data/User /opt/vscode-server/extensions /opt/vscode-cli
 export VSCODE_CLI_DATA_DIR=/opt/vscode-cli
-export SUDO_USER="${SUDO_USER:-vscode}"
-export SUDO_PS1=1
 export PROMPT_DIRTRIM=2
-export PS1='\u@\h:\w\$ '
+export PS1='\u@\dev:\w\$ '
 
-exec code \
+code \
   serve-web \
   --host 0.0.0.0 \
   --port 8080 \
