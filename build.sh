f24f5daa70a032836d8bfd1bee69cc62b961d183
David Dadon <david@dadon.fr>
2026-08-11 10:34:36 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker tag ddadon/dev ddadon/dev:previous >/dev/null 2>&1 || true
-docker build --file docker/Dockerfile --tag ddadon/dev .
+docker tag ddadon/dev:current ddadon/dev:previous
+docker build --file docker/Dockerfile --tag ddadon/dev:current .
+docker push ddadon/dev:previous
+docker push ddadon/dev:current
 
-docker tag ddadon/gitclient ddadon/gitclient:previous >/dev/null 2>&1 || true
-docker build --file docker/Git.Dockerfile --tag ddadon/gitclient .
+docker tag ddadon/gitclient:current ddadon/gitclient:previous
+docker build --file docker/Git.Dockerfile --tag ddadon/gitclient:current .
+docker push ddadon/gitclient:previous
+docker push ddadon/gitclient:current
 
-docker tag ddadon/azureclient ddadon/azureclient:previous >/dev/null 2>&1 || true
-docker build --file docker/Azure.Dockerfile --tag ddadon/azureclient .
+docker tag ddadon/azureclient:current ddadon/azureclient:previous
+docker build --file docker/Azure.Dockerfile --tag ddadon/azureclient:current .
+docker push ddadon/azureclient:previous
+docker push ddadon/azureclient:current
+
+docker image prune --force
