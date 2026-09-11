f994385bdb3ad547a725fb0bd7abea1ee93fa1a7
David Dadon <david@dadon.fr>
2026-09-11 16:31:58 +0000

    Autosave normal-mode text changes
    Handle TextChanged so undo, redo, and other Normal-mode edits are persisted by the guarded native autosave.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,7 +35,7 @@ vim.o.wrap = true
 vim.opt.shortmess:append('IscWa')
 
 -- Autosave
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufLeave', 'FocusLost' }, {
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged', 'BufLeave', 'FocusLost' }, {
     group = vim.api.nvim_create_augroup('ConfigAutosave', { clear = true }),
     nested = true,
     callback = function(args)
